451ad47d8d482fbd2ab2ad5de464e28d3e0ab6e6
farihaorna23 <farihaorna@gmail.com>
2022-06-13 19:36:28 -0400

    addegunctionality to the twiiter button. It tweets the quote and author when clicked

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const quoteContainer = document.getElementById("quote-container");
 const newQuoteButton = document.getElementById("new-quote");
+const twitterButton = document.getElementById("twitter");
+console.log(twitterButton);
 const quote = document.getElementById("quote");
 const author = document.getElementById("author");
 //making it global so that all functions can use it.
@@ -12,7 +14,7 @@ function newQuote() {
   return quote; //will return both quote & author
 }
 
-//Get quotes from API
+// Get quotes from API
 async function getQuotes() {
   const apiUrl = "https://type.fit/api/quotes";
   try {
@@ -25,11 +27,29 @@ async function getQuotes() {
 }
 
 newQuoteButton.addEventListener("click", function() {
-  let getQuote = newQuote(); //will have both quote & author
+  let getQuote = newQuote(); //An object. It will have both quote & author
+  console.log(getQuote);
+  ////check quote length to determine styling
+  if (getQuote["text"].length > 100) {
+    quote.classList.add("long-quote");
+  } else {
+    quote.classList.remove("long-quote");
+  }
   quote.textContent = getQuote["text"];
-  author.textContent = getQuote["author"];
-  // author.textContent = getQuote["author"];
+  if (!getQuote["author"]) {
+    author.textContent = "Unknown";
+  } else {
+    author.textContent = getQuote["author"];
+  }
 });
 
+//Tweet Quote
+function tweetQuote() {
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quote.textContent} - ${author.textContent}`;
+  //_blank will open twiiter in a new tab
+  window.open(twitterUrl, "_blank");
+}
+
+twitterButton.addEventListener("click", tweetQuote);
 //on load
 getQuotes();
